4ad5c9b52f2630768964dc4f5a9bd2a28d1f595a
MrFr4nkTv <114507607+MrFr4nkTv@users.noreply.github.com>
2022-11-01 03:37:18 -0700

    Add files via upload
    Proyecto Final

---
+++ Catrina/src/mx/itson/catrina/enumerador/Tipo.java
@@ -0,0 +1,15 @@
+
+package mx.itson.catrina.enumerador;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ *
+ * @author Frank
+ */
+public enum Tipo {
+    @SerializedName("1")
+    DEPOSITO,
+    @SerializedName("2")
+    RETIRO
+}
